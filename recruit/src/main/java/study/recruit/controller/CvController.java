cf298f1a3a8816388bd34edd34c3297090c9d41c
atgmep <whalebun@gmail.com>
2019-03-23 20:19:27 +0700

    .

--- recruit/src/main/java/study/recruit/controller/CvController.java
+++ recruit/src/main/java/study/recruit/controller/CvController.java
@@ -1,8 +1,25 @@
 package study.recruit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import study.recruit.model.Response;
+import study.recruit.service.CVService;
 
 @Controller
 public class CvController extends AbstractController {
+
+
+    private CVService cvService;
+
+    public CvController(CVService cvService) {
+        this.cvService = cvService;
+    }
+
+    @GetMapping("/api/cv/detail")
+    public String getCvDetail(Integer cvId) {
+        return gson.toJson(cvService.getCvDetailCan(cvId));
+    }
+
+
 
 }
